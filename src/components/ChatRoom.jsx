3192f02601b48509e5751d56a1e2fde75f5d05ed
pulkitgangwar <pulkitgangwar2016@gmail.com>
2021-06-03 19:57:03 +0530

    added basic chatengine in chatroom component

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -1,22 +1,59 @@
-import { Button } from "@chakra-ui/button";
-import { Center } from "@chakra-ui/layout";
-import { Box } from "@chakra-ui/layout";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Button, ChatEngine, getOrCreateChat } from "react-chat-engine";
+import { useAuth } from "../contexts/auth.context";
+import { signUpWithChatEngine } from "../utils/login";
 import { auth } from "../firebase";
 
 const ChatRoom = () => {
+  const { user } = useAuth();
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    const getUser = async () => {
+      await signUpWithChatEngine(user);
+    };
+    getUser();
+  }, [user]);
+
   const handleLogOut = () => {
     auth.signOut();
   };
+
+  const createDirectChat = (creds) => {
+    getOrCreateChat(
+      creds,
+      { is_direct_chat: true, usernames: [username] },
+      () => setUsername("")
+    );
+  };
+
+  const renderChatForm = (creds) => {
+    return (
+      <div>
+        <input
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button onClick={() => createDirectChat(creds)}>Create</button>
+      </div>
+    );
+  };
+
   return (
-    <Box>
-      <Center>
-        <Box>This is the Chat Room</Box>
-        <Button colorScheme="red" onClick={handleLogOut}>
-          Log Out
-        </Button>
-      </Center>
-    </Box>
+    <>
+      <ChatEngine
+        height="100vh"
+        projectID={process.env.REACT_APP_PROJECT_ID}
+        userName={user.email}
+        userSecret={user.uid}
+        renderNewChatForm={(creds) => renderChatForm(creds)}
+      />
+
+      <Button colorScheme="red" onClick={handleLogOut}>
+        Log Out
+      </Button>
+    </>
   );
 };
 
