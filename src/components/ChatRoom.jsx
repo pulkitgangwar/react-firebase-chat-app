b6d3f5998cf5b96e12dfb917c1123baece718c71
pulkitgangwar <pulkitgangwar2016@gmail.com>
2021-06-03 08:39:24 +0530

    used auth context

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -1,7 +1,23 @@
+import { Button } from "@chakra-ui/button";
+import { Center } from "@chakra-ui/layout";
+import { Box } from "@chakra-ui/layout";
 import React from "react";
+import { auth } from "../firebase";
 
 const ChatRoom = () => {
-  return <div>This is the chat room</div>;
+  const handleLogOut = () => {
+    auth.signOut();
+  };
+  return (
+    <Box>
+      <Center>
+        <Box>This is the Chat Room</Box>
+        <Button colorScheme="red" onClick={handleLogOut}>
+          Log Out
+        </Button>
+      </Center>
+    </Box>
+  );
 };
 
 export default ChatRoom;
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,20 @@
+import { Button } from "@chakra-ui/button";
+import { Center } from "@chakra-ui/layout";
 import React from "react";
+import firebase, { auth } from "../firebase";
 
 const SignIn = () => {
-  return <div>This is the signin component</div>;
+  const handleSignIn = () => {
+    auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  };
+
+  return (
+    <Center>
+      <Button colorScheme="teal" onClick={handleSignIn}>
+        Sign In With Google
+      </Button>
+    </Center>
+  );
 };
 
 export default SignIn;
